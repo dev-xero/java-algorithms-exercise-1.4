b8fdeacaec7c1d6607fe773464c8cbd04e0ef53b
Xero kun <elo48858@gmail.com>
2023-06-13 13:22:57 +0100

    `FEAT` Implemented faster num pairs algorithm

--- src/Algorithms/NumPairs.java
+++ src/Algorithms/NumPairs.java
@@ -1,5 +1,9 @@
 package Algorithms;
 
+import Searching.BinarySearch;
+
+import java.util.Arrays;
+
 public class NumPairs {
     public int count(int[] theArray) {
         int cnt = 0;
@@ -13,10 +17,26 @@ public class NumPairs {
         return cnt;
     }
 
+    public int countFast(int[] theArray) {
+        Arrays.sort(theArray);
+        int cnt = 0;
+        BinarySearch search = new BinarySearch();
+
+        for (int i = 0; i < theArray.length; i++) {
+            int index = search.rank(theArray, theArray[i]);
+
+            if (index > i) { cnt++; }
+        }
+
+        return cnt;
+    }
+
     public static void main(String[] args) {
         int[] testArray = new int[] {1, 2, 3, 4, 1, 4};
         int pairCount = new NumPairs().count(testArray);
+        int fastPairCount = new NumPairs().countFast(testArray);
 
-        System.out.println(pairCount);
+        System.out.printf("Normal - %d\n", pairCount);
+        System.out.printf("Fast - %d\n", fastPairCount);
     }
 }
