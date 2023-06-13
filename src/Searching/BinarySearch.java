6be10b8518cee497f6345e1f8e9caacc5a284580
Xero kun <elo48858@gmail.com>
2023-06-13 14:02:41 +0100

    `FEAT` Modified binary search to return the smallest index of a match

--- src/Searching/BinarySearch.java
+++ src/Searching/BinarySearch.java
@@ -1,8 +1,6 @@
 package Searching;
 
-
 public class BinarySearch {
-
     public int rank(int[] theArray, int key) {
         // Implements binary search algorithm
         int lo = 0;
@@ -25,13 +23,34 @@ public class BinarySearch {
             }
         }
 
-
         return -1;
     }
 
+    public int rankClosest(int[] theArray, int key) {
+        // Returns the element with the smallest index that matches the key
+        int index = -1;
+        int left = 0;
+        int right = theArray.length - 1;
 
-    // ----------------------------------------------------------------------------------------------------
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
+            if (theArray[mid] == key) {
+                index = mid;
+                right = mid - 1;
+                continue;
+            }
+
+            if (theArray[mid] > key) {
+                right = mid - 1;
+                continue;
+            }
+
+            left = mid + 1;
+        }
+
+        return index;
+    }
 
     public static void main(String[] args) {
         // Testing
@@ -39,7 +58,6 @@ public class BinarySearch {
         BinarySearch search = new BinarySearch();
 
         System.out.println(search.rank(testArray, -4));
+        System.out.println(search.rankClosest(testArray, 7));
     }
-
 }
-
