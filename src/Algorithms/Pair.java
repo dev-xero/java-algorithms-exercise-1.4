1921b0d75eeddf6272d2c057537d10ce6b612074
Xero kun <elo48858@gmail.com>
2023-06-14 14:33:17 +0100

    `FEAT` Implemented algorithm to find the farthest pair in a 1D-array

--- src/Algorithms/Pair.java
+++ src/Algorithms/Pair.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Pair {
     public double[] closest(double[] theArray) {
-        // Returns a pair of adjacent ints whose absolute difference is no larger than any other pair
+        // Returns a pair of adjacent doubles whose absolute difference is no larger than any other pair
         Arrays.sort(theArray);
 
         double minDiff = Integer.MAX_VALUE;
@@ -23,12 +23,34 @@ public class Pair {
         return pair;
     }
 
+    public double[] farthest(double[] theArray) {
+        // Returns a pair of adjacent doubles whose absolute difference is no smaller than any other pair
+        double maxDiff = Integer.MIN_VALUE;
+        double[] pair = new double[] {theArray[0], theArray[1]};
+
+        for (int i = 1; i < theArray.length; i++) {
+            double curr = theArray[i];
+            double prev = theArray[i-1];
+            double currDiff = Math.abs(curr - prev);
+
+            if (currDiff > maxDiff) {
+                maxDiff = currDiff;
+                pair = new double[] {prev, curr};
+            }
+        }
+
+        return pair;
+    }
+
     public static void main(String[] args) {
         // Testing
         double[] testArray = new double[] {1.0, 2.0, 3.3, 6.0, 8.0, 11.0};
         Pair pair = new Pair();
 
         double[] theClosestPair = pair.closest(testArray);
-        System.out.printf("[%f, %f]", theClosestPair[0], theClosestPair[1]);
+        double[] theFarthestPair = pair.farthest(testArray);
+
+        System.out.printf("Closest Pair - [%f, %f]\n", theClosestPair[0], theClosestPair[1]);
+        System.out.printf("Farthest Pair - [%f, %f]\n", theFarthestPair[0], theFarthestPair[1]);
     }
 }
