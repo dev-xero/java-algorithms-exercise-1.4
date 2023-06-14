3a3d3f7ff5af26170d71cc8e994092e8c7f21601
Xero kun <elo48858@gmail.com>
2023-06-14 14:23:36 +0100

    `FEAT` Implemented algorithm to compute the closest pair in a 1D-array

---
+++ src/Algorithms/ClosestPair.java
@@ -0,0 +1,34 @@
+package Algorithms;
+
+import java.util.Arrays;
+
+public class ClosestPair {
+    public double[] pair(double[] theArray) {
+        // Returns a pair of adjacent ints whose absolute difference is no larger than any other pair
+        Arrays.sort(theArray);
+
+        double minDiff = Integer.MAX_VALUE;
+        double[] pair = new double[] {theArray[0], theArray[1]};
+
+        for (int i = 1; i < theArray.length; i++) {
+            double prev = theArray[i-1];
+            double currDiff = Math.abs(theArray[i] - prev);
+
+            if (currDiff < minDiff) {
+                minDiff = currDiff;
+                pair = new double[] {prev, theArray[i]};
+            }
+        }
+
+        return pair;
+    }
+
+    public static void main(String[] args) {
+        // Testing
+        double[] testArray = new double[] {1.0, 2.0, 3.3, 6.0, 8.0, 11.0};
+        ClosestPair closestPair = new ClosestPair();
+
+        double[] theClosestPair = closestPair.pair(testArray);
+        System.out.printf("[%f, %f]", theClosestPair[0], theClosestPair[1]);
+    }
+}
